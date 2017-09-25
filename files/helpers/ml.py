2169baef5e2aed722568b6f74c7b1f955b3f711a
Remco Troost <remcotroost@outlook.com>
2017-09-25 19:40:17 +0200

    Optimized leaves detection

--- files/helpers/ml.py
+++ files/helpers/ml.py
@@ -1,4 +1,58 @@
+import numpy as np
+import tensorflow as tf
+import offshoot
+
+
 def load_image_into_numpy_array(image):
   (im_width, im_height) = image.size
   return np.array(image.getdata()).reshape(
       (im_height, im_width, 3)).astype(np.uint8)
+
+def load_graph(frozen_graph_filename):
+    # We load the protobuf file from the disk and parse it to retrieve the
+    # unserialized graph_def
+    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+    # Then, we can use again a convenient built-in function to import a graph_def into the
+    # current default Graph
+    with tf.Graph().as_default() as graph:
+        tf.import_graph_def(
+            graph_def,
+            name="",
+        )
+
+    return graph
+
+def predict(tf_model, frame):
+
+    categories = {1: {'id': 1, 'name': 'leaves'}}
+    image = np.expand_dims(frame, axis=0)
+
+    image_tensor = tf_model.get_tensor_by_name('image_tensor:0')
+    detection_boxes = tf_model.get_tensor_by_name('detection_boxes:0')
+    detection_scores = tf_model.get_tensor_by_name('detection_scores:0')
+    detection_classes = tf_model.get_tensor_by_name('detection_classes:0')
+    num_detections = tf_model.get_tensor_by_name('num_detections:0')
+
+    output = [detection_boxes, detection_scores, detection_classes, num_detections]
+
+    with tf.Session(graph=tf_model) as sess:
+        (boxes, scores, classes, num) = sess.run(output, feed_dict={image_tensor: image})
+
+        for i,b in enumerate(boxes[0]):
+            if classes[0][i] == 1:
+                class_name = categories[classes[0][i]]['name']
+                if scores[0][i] >= 0.5:
+                    top = boxes[0][i][0] * 576
+                    left = boxes[0][i][1] * 1024
+                    bottom = boxes[0][i][2] * 576
+                    right = boxes[0][i][3] * 1024
+
+                    box_left = (top, left), (bottom, left)
+                    box_right = (top, right), (bottom, right)
+                    box_top = (top, left), (top, right)
+                    box_bottom = (bottom, left), (bottom, right)
+
+                    return box_left, box_right, box_top, box_bottom
--- files/serpent_Cloney_game_agent.py
+++ files/serpent_Cloney_game_agent.py
@@ -6,7 +6,8 @@ from serpent.machine_learning.context_classification.context_classifiers.cnn_inc
 from .helpers.ml import *
 
 import offshoot
-import tensorflow as tf
+
+
 import numpy as np
 
 import time
@@ -36,15 +37,16 @@ class SerpentCloneyGameAgent(GameAgent):
 
         self.machine_learning_models["context_classifier"] = context_classifier
 
+        self.tf_model = load_graph(f'{self.plugin_path}/SerpentCloneyGameAgentPlugin/files/ml_models/leaves_detection/frozen_inference_graph.pb')
+
+
         self.current_run = 0
         self.current_run_started_at = None
 
         self.last_run_duration = 0
 
-        # Leaves object detection
-
-        self.ml_categories = {1: {'id': 1, 'name': 'leaves'}}
         self.detection_graph = self.load_leaves_model()
+        self.leaves = ""
 
     def handle_play(self, game_frame):
         context = self.machine_learning_models["context_classifier"].predict(game_frame.frame)
@@ -66,8 +68,8 @@ class SerpentCloneyGameAgent(GameAgent):
     def handle_play_context_game_world(self, game_frame):
         # self.input_controller.click(y=288, x=512)
         self.display_game_agent_state(context="GAME_WORLD")
-        if len(self.game_frame_buffer.frames) > 1:
-            self.detect_leaves(detection_graph=self.detection_graph,category_index=self.ml_categories)
+        if len(self.game_frame_buffer.frames) > 0:
+            self.leaves = predict(tf_model=self.tf_model, frame=self.game_frame_buffer.frames[1].frame)
 
     def handle_play_context_game_over(self, game_frame):
         for i, game_frame in enumerate(self.game_frame_buffer.frames):
@@ -121,54 +123,5 @@ class SerpentCloneyGameAgent(GameAgent):
         print("")
         print(f"\nLAST RUN: {self.current_run - 1}")
         print(f"LAST RUN DURATION: {self.last_run_duration} seconds")
-
-    def load_leaves_model(self):
-        detection_graph = tf.Graph()
-
-        with detection_graph.as_default():
-            od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(f'{self.plugin_path}/SerpentCloneyGameAgentPlugin/files/ml_models/leaves_detection/frozen_inference_graph.pb', 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
-
-        return detection_graph
-
-    def detect_leaves(self, detection_graph=None, category_index=None):
-        if detection_graph is None:
-            return
-
-        with detection_graph.as_default():
-            with tf.Session(graph=detection_graph) as sess:
-                #for i, game_frame in enumerate(self.game_frame_buffer.frames):
-                    # Definite input and output Tensors for detection_graph
-                image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-                # Each box represents a part of the image where a particular object was detected.
-                detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-                # Each score represent how level of confidence for each of the objects.
-                # Score is shown on the result image, together with the class label.
-                detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
-                detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
-                num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-                # the array based representation of the image will be used later in order to prepare the
-                # result image with boxes and labels on it.
-                #image_np = load_image_into_numpy_array(frame)
-                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-                image = np.expand_dims(self.game_frame_buffer.frames[0].frame, axis=0)
-                # Actual detection.
-                (boxes, scores, classes, num) = sess.run(
-                  [detection_boxes, detection_scores, detection_classes, num_detections],
-                  feed_dict={image_tensor: image})
-                # Visualization of the results of a detection.
-
-                for i,b in enumerate(boxes[0]):
-                    if classes[0][i] == 1:
-                        class_name = category_index[classes[0][i]]['name']
-                        if scores[0][i] >= 0.5:
-                            ymin = boxes[0][i][0] * 576
-                            xmin = boxes[0][i][1] * 1024
-                            ymax = boxes[0][i][2] * 576
-                            xmax = boxes[0][i][3] * 1024
-                            # If x1 > x_of_cloney
-                            # If cloney is inside box then x
-                            print(f"DETECTED | SCORE { scores[0][i] } | POSITION {ymin}, {xmin}, {ymax}, {xmax}\n")
+        print("")
+        print(f"DETECTED LEAVES: {self.predictions}")
